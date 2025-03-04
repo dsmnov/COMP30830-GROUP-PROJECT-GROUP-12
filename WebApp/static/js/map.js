5a5ce03b9e05aaba76011d8b0e8b798ebbd4bed6
dsmnov <denis.semenov@ucdconnect.ie>
2025-03-04 00:26:26 +0000

    Tweaked Color settings, HSL is defined and dynamically updated by occupancy rather than defined as a const, this allows for edge case handling where bad data leads to a 0/0 division in which case the bike color defaults to white.

--- WebApp/static/js/map.js
+++ WebApp/static/js/map.js
@@ -95,8 +95,10 @@ async function updateMarkersIcons() {
   markers.forEach(marker => {
     const stationData = availData.find(data => data.number === marker.stationId);
     const occupancy = stationData.available_bikes / (stationData.available_bikes + stationData.available_bike_stands);
-    let hue;
     
+    let hue;
+    let saturation;
+    let lightness;
     if (occupancy >= 0.80) {
       hue = occupancy * 220;
     } else if (occupancy <= 0.15) {
@@ -104,9 +106,15 @@ async function updateMarkersIcons() {
     } else {
       hue = occupancy * 160;
     }
+    saturation = 80;
+    lightness = 70;
+
+    if (isNaN(occupancy)) {
+      hue = 0
+      saturation = 100;
+      lightness = 100;
+    }
     
-    const saturation = 80;
-    const lightness = 70;
     const svgColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 
     const pinSvgString = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38 38" width="38" height="38"><path id="background" d="M19,1 C12,1 7,6 7,13 C7,22 19,37 19,37 C19,37 31,22 31,13 C31,6 26,1 19,1 Z" fill="${svgColor}" stroke="black" stroke-width="1"/><g id="separateur-titre" transform="translate(8.7,9) scale(0.55)"><path d="M29.85,7.78a7.23,7.23,0,0,0,-2.29,0.38l-2,-6A2.73,2.73,0,0,0,22.93,0.32H21.22a1.05,1.05,0,0,0,0,2.1h1.71a0.66,0.66,0,0,1,0.61,0.44l0.29,0.88H15l-0.14,-0.43l0.47,-0.06a0.84,0.84,0,0,0,0.73,-0.82V2.35a1.16,1.16,0,0,0,-1,-1.17L12.11,0.9a1.18,1.18,0,0,0,-1.28,1.31l0.1,0.85a0.73,0.73,0,0,0,0.27,0.49a0.75,0.75,0,0,0,0.54,0.15l1,-0.13l0.32,1L10.81,8.32a6.92,6.92,0,0,0,-2.7,-0.54a7.11,7.11,0,1,0,7,8.16h3a1.08,1.08,0,0,0,0.81,-0.37L25.3,8l0.38,1.1a7.1,7.1,0,1,0,4.17,-1.36ZM13.93,7.19l2.2,6.65h-1a7.11,7.11,0,0,0,-2.51,-4.45Zm-2.41,4A5,5,0,0,1,13,13.84H10ZM8.11,19.9a5,5,0,0,1,0,-10a5.08,5.08,0,0,1,1.61,0.27l-2.51,4.2a1.05,1.05,0,0,0,0.9,1.59H13A5,5,0,0,1,8.11,19.9Zm10,-6.65L15.7,5.84h8.71Zm11.7,6.65a5,5,0,0,1,-3.45,-8.64a12.08,12.08,0,0,0,2.69,4.36a1.07,1.07,0,0,0,0.76,0.32a1.05,1.05,0,0,0,0.75,-1.78a9.8,9.8,0,0,1,-2.23,-3.64l-0.13,-0.37a5,5,0,1,1,1.61,9.75Z"/></g></svg>`;
