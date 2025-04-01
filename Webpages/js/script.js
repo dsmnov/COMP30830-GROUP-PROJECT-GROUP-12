0ff361edddaa0a732d12c755889764a5d2d4ce1e
Diviyya Shree Iyappan <diviyya.iyappan@ucdconnect.ie>
2025-04-01 16:56:52 +0100

    Frontend

---
+++ Webpages/js/script.js
@@ -0,0 +1,16 @@
+const menuToggle = document.getElementById('menuToggle');
+const mobileNav = document.getElementById('mobileNav');
+const closeMenu = document.getElementById('closeMenu');
+const body = document.body;
+
+// Open Mobile Menu
+menuToggle.addEventListener('click', () => {
+    mobileNav.classList.add('active');
+    body.style.overflow = 'hidden'; // Disable scroll
+});
+
+// Close Mobile Menu
+closeMenu.addEventListener('click', () => {
+    mobileNav.classList.remove('active');
+    body.style.overflow = ''; // Enable scroll
+});
